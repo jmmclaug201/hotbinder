cd123711389191645d67ea4e1892bfff4f0d2445
Joe McLaughlin <mclaughlinj201@gmail.com>
2025-02-20 13:36:24 -0500

    Production setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,4 +85,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Temporarily allow www-03 as a host
+  config.hosts << "www-03.springcarnival.org"
 end
